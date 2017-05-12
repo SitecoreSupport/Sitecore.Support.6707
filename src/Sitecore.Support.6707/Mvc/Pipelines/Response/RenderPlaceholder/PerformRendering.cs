2a6f819c6f3367d5ef5753735664a136071b6c54
Anton Kechashin <atk@sitecore.net>
2017-05-12 11:22:42 +0300

    Initial commit

---
+++ src/Sitecore.Support.6707/Mvc/Pipelines/Response/RenderPlaceholder/PerformRendering.cs
@@ -0,0 +1,25 @@
+﻿using Sitecore.Diagnostics;
+using Sitecore.Mvc.Exceptions;
+using Sitecore.Mvc.Presentation;
+
+namespace Sitecore.Support.Mvc.Pipelines.Response.RenderPlaceholder
+{
+    public class PerformRendering : Sitecore.Mvc.Pipelines.Response.RenderPlaceholder.PerformRendering
+    {
+        protected override RecursionStack CreateCyclePreventer(string placeholderName, Rendering rendering)
+        {
+            Assert.IsNotNull(rendering, "rendering");
+            #region Changed code
+            string str1 = rendering.UniqueId.ToString(); // Use Unique rendering ID instead of Renderer
+            #endregion
+            string str2 = placeholderName + "-" + str1;
+            string details = "[" + placeholderName + "-" + str1 + "- {" + rendering.UniqueId + "}" + "]";
+            RecursionStack recursionStack = new RecursionStack("Rendering", str2, details);
+            if (recursionStack.GetCount("Rendering", str2) > 1)
+            {
+                throw new CyclicRenderingException("A rendering has been recursively embedded within itself. Embedding trail: " + recursionStack.GetTrail("Rendering", " --> "));
+            }
+            return recursionStack;
+        }
+    }
+}
---
+++ src/Sitecore.Support.6707/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.6707")]
+[assembly: AssemblyProduct("Sitecore.Support.6707")]
+[assembly: ComVisible(false)]
